9cc62048181f84aa3a905ba8e7381782dba2ad31
Dandertuts <68716595+Dandertuts@users.noreply.github.com>
2020-07-24 18:35:30 -0400

    Add files via upload

--- New game/TileMap.gd
+++ New game/TileMap.gd
@@ -1,11 +1,20 @@
 extends TileMap
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("click"):
-		var tile = world_to_map(event.position)
-		set_cell(tile.x, tile.y, -1)
-		
-	elif event.is_action_pressed("right_click"):
-		var tile= world_to_map(event.position)
-		set_cell(tile.x, tile.y, 0);
-	pass 
+func _process(delta):
+	
+	
+	if Input.is_action_pressed("right_click"):
+		var MousePos = get_global_mouse_position()
+		var loc = world_to_map(MousePos)
+		var cell = get_cell(loc.x, loc.y)
+		if cell == -1:
+			set_cell(loc.x, loc.y, 1)
+			
+			
+	if Input.is_action_pressed("click"):
+		var MousePos = get_global_mouse_position()
+		var loc = world_to_map(MousePos)
+		var cell = get_cell(loc.x, loc.y)
+		if cell == 1:
+			set_cell(loc.x, loc.y, -1)
+	
