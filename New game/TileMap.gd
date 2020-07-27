0c9b111bc535d1bb279533c3661ba70ee2d13c5e
JamieLinchpin <52760067+Springstorm53a@users.noreply.github.com>
2020-07-26 22:07:52 -0400

    AHHHHHH I DID IT AIHBFIHAbvihybda
    I DID IT I MADE PROCEDURALLY GENERATED CAVES AOJBVDAOLZJB AH804888

--- New game/TileMap.gd
+++ New game/TileMap.gd
@@ -1,7 +1,30 @@
 extends TileMap
 
-func _process(delta):
+var XCave = 1
+var YCave = 1
+
+
+
+var SelectedCell
+
+func CaveGen():
+	YCave = rand_range(1, 50)
+	XCave = rand_range(1, 50)
+	SelectedCell = get_cell(XCave - 1, YCave)
+	if  SelectedCell == 0:
+		print("yep, that's dirt")
+	else:
+		set_cell(XCave, YCave, -1)
+
+func _ready():
+	CaveGen()
 	
+		
+
+
+
+func _process(delta):
+	CaveGen()
 	
 	if Input.is_action_pressed("right_click"):
 		var MousePos = get_global_mouse_position()
@@ -20,74 +43,9 @@ func _process(delta):
 	
 #############################################################
 
-enum TILES {Dirt, Air}
 
-var current_map_size = Vector2(50, 50)
-var total_walkers = []
-var spawn_chance = 0.005
-var percentage_to_fill = 0.35
 
-class walker extends Reference:
-	var pos
-
-	func _init(starting_position):
-		pos = starting_position
-		
-func _ready():
-	randomize()
-	for x in current_map_size.x:
-		for y in current_map_size.y:
-			set_cell(x,y, 0)
-	move_walkers()
 	
-func move_walkers():
-		var iterations = 0
-		var pos = Vector2((current_map_size.x / 2), current_map_size.y / 2)
-		var w = walker.new(pos)
-		total_walkers.append(w)
-		while iterations < 10000:
-			for walk in total_walkers:
-				var num = randi() % 4
-				match num:
-					0:
-						walk.pos += Vector2(1, 0)
-					1:
-						walk.pos += Vector2(0, 1)
-					2:
-						walk.pos += Vector2(-1, 0)
-					3:
-						walk.pos += Vector2(0, -1)
-				
-				walk.pos.x = clamp(walk.pos.x, 1, current_map_size.x - 2)
-				walk.pos.y = clamp(walk.pos.x, 1, current_map_size.y - 2)
-			
-				set_cell(walk.pos.x, walk.pos.y, TILES.Dirt)
-				#chance to spawn near new walker
-				if randf() < spawn_chance and total_walkers.size() < 10:
-					var y = walker.new(walk.pos)
-					total_walkers.append(y)
-					
-				#chance to delete walker
-				if randf() < spawn_chance / 2 and total_walkers.size() > 1:
-					total_walkers.erase(walk)
-	
-		iterations += 1
-		yield(get_tree(), "idle_frame")
-		
-		if count_floors() / (current_map_size.x *current_map_size.y ) > percentage_to_fill:
-			pass
-		
-	
-func count_floors():
-	var floor_count = 0
-	for x in current_map_size.x:
-		for y in current_map_size.y:
-			var current_cell =  get_cell(x,y)
-			if current_cell == TILES.Floor:
-				floor_count += 1
-				
-		return floor_count   
-		
 		  
 		
 
